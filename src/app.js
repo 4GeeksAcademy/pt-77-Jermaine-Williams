ec34b0c196c893a0942889a099155b4128fe34b4
Jermaine Williams <wjermaine608@gmail.com>
2025-02-20 01:12:59 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+ 
+    const whoArray =["The dog", " My grandma", "The police", "My Cat"]
+    const actionArray =["ate", "peed", "Ripped", "broke"]
+    const whatArray =["my homework", "my card", "the remote"]
+    const whenArray =["before the class", "when I was sleeping","while I was in the shower","during break","while I was praying"]
+
+    let whoIndex = Math.floor(Math.random() * whoArray.length)
+    let actionIndex = Math.floor(Math.random() * actionArray.length)
+    let whatIndex = Math.floor(Math.random() * whatArray.length)
+    let whenIndex = Math.floor(Math.random() * whenArray.length)
+
+    const who = document.querySelector(".who")
+    const action = document.querySelector(".action")
+    const what = document.querySelector(".what")
+    const when = document.querySelector(".when")
+
+    who.innerHTML = whoArray[whoIndex]
+    action.innerHTML = actionArray[actionIndex]
+    what.innerHTML = whatArray[whatIndex]
+    when.innerHTML = whenArray[whenIndex]
+
+
+    console.log("Hello Rigo from the console!");
+
+  }
+
+
+
+
+
